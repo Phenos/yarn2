2236bdc51bf783bca1a761f20a0f1191145f0803
mathieu <mathieu.sylvain@phenos.ca>
2016-05-14 13:10:18 -0400

    Added localStorage persistance for store

--- src/js/store.jsx
+++ src/js/store.jsx
@@ -1,8 +1,8 @@
-
+import { persistStore, autoRehydrate } from 'redux-persist';
 import { createStore } from 'redux';
-
 import reducer from './reducer.jsx';
 
-let store = createStore(reducer);
+let store = createStore(reducer, undefined, autoRehydrate());
+persistStore(store);
 
 export default store;
