e22bf06886d691fe35c5c5db61dbaaeeff355a8e
mathieu <mathieu.sylvain@phenos.ca>
2016-05-18 21:34:33 -0400

    Fixed mutable state

--- src/js/Containers/yarn.jsx
+++ src/js/Containers/yarn.jsx
@@ -14,6 +14,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import DevTools from './devTools.jsx';
+
 import auth from '../services/auth.jsx';
 
 class Yarn extends React.Component {
